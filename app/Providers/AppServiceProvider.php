3e4f692f0bd3780b528d6d268e603b2e4e61cc44
krymenghuy <menghuykry@gmail.com>
2025-07-15 22:44:33 +0700

    Deploy Laravel to Vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Company;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('backends.layouts.sidebar', function ($view) {
+            $view->with('company', Company::first());
+        });
     }
 }
--- resources/views/backends/layouts/sidebar.blade.php
+++ resources/views/backends/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);">
     <!-- Brand Logo -->
     <a href="{{ route('admin.home') }}" class="brand-link">
-        <img src="{{ asset($company->photo ?? 'images/default-logo.png') }}" alt="AdminLTE Logo"
+        <img src="{{ asset($company->photo) }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light text-white">DOCUMENT SYSTEM</span>
     </a>
