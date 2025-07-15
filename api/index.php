ac733b75b382f326dc36852b4a9cfaa7b20f2a0d
krymenghuy <menghuykry@gmail.com>
2025-07-16 00:04:41 +0700

    first commit

--- api/index.php
+++ api/index.php
@@ -1,15 +1,4 @@
+
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$request = Illuminate\Http\Request::capture();
-
-$response = $kernel->handle($request);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
