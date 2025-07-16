aa347112142854ae1834e94e8961ab57c40b31c8
krymenghuy <menghuykry@gmail.com>
2025-07-16 15:38:12 +0700

    Fix merge conflict in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\View;
 use App\Models\Company;
-=======
->>>>>>> 9a9aa51486357edfe72c6b3321aafa5821e401bf
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,12 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-<<<<<<< HEAD
         View::composer('backends.layouts.sidebar', function ($view) {
             $view->with('company', Company::first());
         });
-=======
-        //
->>>>>>> 9a9aa51486357edfe72c6b3321aafa5821e401bf
     }
 }
